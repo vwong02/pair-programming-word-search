87d88fce5d9ba3268befc9c92c6f15b9f02c1855
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-22 19:12:56 -0500

    added files

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present (horizontally)", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -32,6 +32,22 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present (vertically)", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'H', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'A', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'P', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'P', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'Y', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'HAPPY')
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,42 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  // Mapping though the letters of the arrays horizontally and join each letter in the arry together without a space (as one word)
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  // If the word is included in the word search horizontally, return true
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  // Create an empty array to transpose the horizontal array to vertical array
+  let verticalJoin = [];
+
+  // Iterate through each column and row
+  for (let i = 0; i < horizontalJoin.length; i++) { //Iterates through the ows of the original grid
+    for (let j = 0; j < horizontalJoin[i].length; j++) { //Iterates through the columns of the current rows
+
+      //Check if there is already a sub-array at the current column index, if not, push into the new array
+      if (!verticalJoin[j]) {
+        verticalJoin.push([]);
+      }
+      // Transpose the horizontalJoin into the verticalJoin
+      verticalJoin[j][i] = horizontalJoin[i][j];
+    }
+  }
+
+  //  Map though the letters of the verticalJoin arrays horizontally and join each letter in the arry together without a space (as one word)
+  verticalJoin = verticalJoin.map((v) => {
+    return v.join('');
+  });
+
+  // Checking if the word is included in the word search in the verticalJoin arrays
+  for (v of verticalJoin) {
+    if (v.includes(word)) {
+      return true;
+    }
+  }
+  // If no matches are found, return false
+  return false;
+};
+
+module.exports = wordSearch;
